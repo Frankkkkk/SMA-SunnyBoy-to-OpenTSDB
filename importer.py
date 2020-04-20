bd904d0d2b2b85e77b99cdda09748471663a19ac
Frank Villaro-Dixon <frank@vi-di.fr>
2020-04-20 21:01:12 +0200

    Stable v0.1

--- importer.py
+++ importer.py
@@ -7,42 +7,39 @@ import requests
 import time
 
 SMA_SUNNYBOY_HOST = os.environ['SMA_SUNNYBOY_HOST']
-#OPEN_TSDB_HOST = os.environ['OPENTSDB_HOST']
-#OPEN_TSDB_METRIC = os.environ['OPEN_TSDB_METRIC']
-#OPEN_TSDB_TAGS = json.loads(os.environ['OPEN_TSDB_TAGS'])
+OPEN_TSDB_HOST = os.environ['OPEN_TSDB_HOST']
+OPEN_TSDB_METRIC = os.environ['OPEN_TSDB_METRIC']
+OPEN_TSDB_POINT_NAME = os.environ['OPEN_TSDB_POINT_NAME']
+
 
 def get_data_from_sunnyboy(sma_host):
     data = requests.get(f'http://{sma_host}/dyn/getDashLogger.json', verify=False)
     energies = data.json()['result']['0199-B32FB7BF']['7000']['1']
     return energies
 
+
 def send_data_to_opentsdb(data):
-    pts = []
     for d in data:
-        pt = {
-            "metric": "energy.electricity.cumulative",
-            "timestamp": int(d['t']),
-            "value": float(d['v']),
-            "tags": {
-               "point": "solar.roof.production"
+        try:
+            pt = {
+                "metric": OPEN_TSDB_METRIC,
+                "timestamp": int(d['t']),
+                "value": float(d['v']),
+                "tags": {
+                   "point": OPEN_TSDB_POINT_NAME,
+                }
             }
-        }
-        print(pt)
-        r = requests.post('http://192.168.10.52:4242/api/put', json=pt)
-        print(r)
-        print(r.status_code)
-
-
+            # XXX send an array instead of scalars
+            r = requests.post('http://{OPEN_TSDB_HOST}/api/put', json=pt)
+        except:
+            print(f'No data for pt {d}. Maybe inverter was down')
 
 
 if __name__ == '__main__':
-    e = get_data_from_sunnyboy('192.168.10.220')
-    send_data_to_opentsdb(e)
-#while True:
-#    data = get_data_from_sunnyboy(SMA_SUNNYBOY_HOST)
-#    send_data_to_opentsdb(data)
-#    time.sleep(10m)
-
+    while True:
+        e = get_data_from_sunnyboy('192.168.10.220')
+        send_data_to_opentsdb(e)
+        time.sleep(60 * 60)
 
 # vim: set ts=4 sw=4 et:
 
